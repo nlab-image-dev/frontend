6b53421c5693269fb810483c23b792659236be81
Y.O <avogf46119@yahoo.co.jp>
2021-09-07 15:08:31 +0900

    User auth base

---
+++ src/js/components/user_auth/AuthArea.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+
+import Auth from './Auth';
+import User from './User';
+import Login from "./Login";
+import Logout from "./Logout";
+import List1 from "./List1";
+
+export default class AuthArea extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/logout" component={Logout} />
+          <Auth>
+            <Switch>
+              <Route exact path="/list1" component={List1} />
+              <Redirect from="/" to="/list1" />
+            </Switch>
+          </Auth>
+        </Switch>
+      </Router>
+    );
+  }
+}
