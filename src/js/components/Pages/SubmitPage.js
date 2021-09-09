51a07a2d2b0972c749c883c48d09ec5e569b3b0c
Y.O <avogf46119@yahoo.co.jp>
2021-09-09 13:31:29 +0900

    フォルダ整理 ページ追加

---
+++ src/js/components/Pages/SubmitPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class SubmitPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>ここは投稿ページ</h1>
+      </div>
+    );
+  }
+}
