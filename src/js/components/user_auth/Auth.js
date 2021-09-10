24d6c30995b3288f0ab5c4c2d1cd72e8f7073ad4
GloUnknown <gloun342708728@gmail.com>
2021-09-10 16:57:14 +0900

    style改善

--- src/js/components/user_auth/Auth.js
+++ src/js/components/user_auth/Auth.js
@@ -3,19 +3,20 @@ import { Redirect } from 'react-router-dom';
 import { useSelector } from "react-redux";
 import { Button } from "@material-ui/core";
 import { Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 function Auth ({children}) {
   const userFetched = useSelector((state) => state.userReducer.fetched);
   if (!userFetched) {
     return (
-      <div>
+      <Container>
         <h1>ログインしてください</h1>
         <Button 
           variant="contained" color="primary" component={Link}
           to="/login">
           ログイン
         </Button>
-      </div>
+      </Container>
     )
   } else {
     return children
